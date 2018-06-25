a9caa9dc147449b4f8ad343c2b77ea34eaeb003a
Jessie Deot <jessiedeot@gmail.com>
2018-06-25 11:19:18 -0700

    Initial commit

--- src/java/sample.java
+++ src/java/sample.java
@@ -1,7 +1,7 @@
 
 
 /**
- * Copyright 2018 SunSpec Alliance
+ * Copyright [yyyy] [name of copyright owner]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
